1bb266db6b72361a5ef5873dd8f5a0cad7d7bf16
Pranavi2002 <66314704+Pranavi2002@users.noreply.github.com>
2021-06-22 21:14:29 +0530

    myapp files

---
+++ session-5/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+	fullname=models.CharField(max_length=100,blank=False)
+	email=models.EmailField(max_length=100,blank=False)
+	username=models.CharField(max_length=100,blank=False)
+	password=models.CharField(max_length=100,blank=False)
+	mobileno=models.CharField(max_length=100,blank=False)
+	location=models.CharField(max_length=100,blank=False)
+	class Meta:
+		db_table = "user_table"
+
+class Content(models.Model):
+	category_choices=(('Education','Education'),('Programming','Programming'),('Others','Others'))
+	category=models.CharField(max_length=100,choices=category_choices,blank=False)
+	title=models.CharField(max_length=100,blank=False)
+	description=models.CharField(max_length=1000,blank=False)
+	link=models.CharField(max_length=100,blank=False)
+	class Meta:
+		db_table = "content_table"
+
